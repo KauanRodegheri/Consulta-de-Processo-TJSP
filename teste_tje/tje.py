9e24547c65d5d0801610b3d4a0fb655db238b361
Kauan Rodegheri <kauan.rodegheri@gmail.com>
2024-10-06 17:28:13 -0300

    processos funcionando

---
+++ teste_tje/tje.py
@@ -0,0 +1,54 @@
+import selenium
+import openpyxl
+from time import sleep
+import selenium.webdriver
+from selenium.webdriver.common.by import By
+
+#ABRIR NAVEGADOR COM SELENIUM
+driver = selenium.webdriver.Chrome()
+driver.get('https://esaj.tjsp.jus.br/cpopg/open.do')
+sleep(1)
+
+#ABRIR ARQUIVO XLSX COM OPENPYXL
+book = openpyxl.load_workbook('teste_tje/processos.xlsx')
+pagina = book['Planilha1']
+
+#PERCORRE AS LINHAS DESEJADAS NA PLANILHA
+
+for linha in pagina.iter_rows(min_row=2, max_row=4, values_only=True):
+    processo_1 = linha[0]
+    status = linha[2]
+    sleep(1)
+    
+    #PASSO A PASSO PARA INSERIR INFORMAÇÕES NO SITE E CONFIRMAR
+    click = driver.find_element(By.ID, 'radioNumeroAntigo').click()
+    sleep(0.5)
+    n_processo = driver.find_element(By.ID, 'nuProcessoAntigoFormatado')
+    sleep(2)
+    n_processo.clear()
+    sleep(0.5)
+    n_processo.send_keys(processo_1)
+    sleep(2)
+    driver.find_element(By.ID, 'botaoConsultarProcessos').click()
+    sleep(2)
+
+    #CAPTURANDO INFORMAÇÕES DOS PROCESSOS E SALVANDO NA PLANILHA
+    data_processo = driver.find_element(By.XPATH, "//td[@class='dataMovimentacao']").text
+    status_processo = driver.find_element(By.XPATH, "//td[@class='descricaoMovimentacao']").text.title()
+    status_processo = status_processo.replace(' ','').split()[0]
+    palavra = ''
+    for letra in status_processo:
+        if letra.isupper():
+            letra = ' ' + letra
+        palavra+=letra
+    for linha in pagina.iter_rows(min_row=2, max_row=5):
+        if linha[0].value == processo_1:
+            linha[2].value = str(palavra)
+            linha[3].value = str(data_processo)
+            break
+    book.save('teste_tje/processos.xlsx')
+    driver.get('https://esaj.tjsp.jus.br/cpopg/open.do')
+    continue
+    
+    
+    
